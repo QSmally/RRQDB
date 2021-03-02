0ab2ae015a505f639a1ffa3764918fce1ab340a8
QSmally <github@qbot.eu>
2021-03-02 19:27:07 +0100

    Implement functional RRQDB design

--- RRQDB.js
+++ RRQDB.js
@@ -13,15 +13,89 @@ class RRQDB extends Connection {
      */
     constructor (Identifier, RRQDB = {}) {
 
-        const PathURL = RRQDB?.Path || "RRQDB";
+        const RROptions = {
+            Path:      "RRQDB.qdb",
+            Memory:    true,
+            Size:      200,
+            BatchSize: 30,
 
-        super(PathURL, {
+            ...RRQDB
+        };
+
+        super(RROptions.Path, {
             Table:         Identifier,
-            FetchAll:      RRQDB.Memory ?? true,
-            CacheMaxSize:  RRQDB.Size ?? 200,
-            SweepInterval: false
+            FetchAll:      RROptions.Memory,
+            SweepInterval: !RROptions.Memory,
+            CacheMaxSize:  RROptions.Size
         });
 
+        /**
+         * Validated RRQDB options.
+         * @name RRQDB#RROptions
+         * @type {RawOptions}
+         * @readonly
+         */
+        Object.defineProperty(this, "RROptions", {
+            enumerable: true,
+            value: RROptions
+        });
+
+    }
+
+
+    /**
+     * Inserts a record into the RRQDB.
+     * @param {String} Key A unique identifier to insert this document as.
+     * @param {Object|Array} Document An event addressed at the key.
+     * @returns {Object} An analytics object.
+     */
+    Insert (Key, Document) {
+        if (!this._Ready)                 throw new Error("RRQDB Connection was terminated.");
+        if (typeof Key !== "string")      throw new TypeError("Key should be a type of String.");
+        if (typeof Document !== "object") throw new TypeError("Document should be a type of Object or Array.");
+
+        const Address = Key.replace(/(\.)/g, "_");
+        if (super.Exists(Address)) throw new Error(`Event '${Address}' already exists.`);
+
+        const Analytics = {
+            Size:     undefined,
+            Batching: 0,
+            Erased:   0
+        };
+
+        const Content = {
+            ...Document,
+            _Inserted: Date.now(),
+            _Batched: false
+        };
+
+        super.Set(Address, Content);
+        const Size = super.Size;
+
+        if (Size > this.RROptions.Size) {
+            const Batch = super.Select()
+            .Order((a, b) => a._Inserted - b._Inserted)
+            .Limit(this.RROptions.Size)
+            .Keys;
+
+            if (Batch.length) {
+                const T = super.Transaction();
+                for (const Item of Batch) super.Set(`${Item}._Batched`, true);
+                T.Commit();
+            }
+
+            Analytics.Batching = Batch.length;
+        }
+
+        if (Size > this.RROptions.Size + this.RROptions.BatchSize) {
+            const Batch = super.Select(Item => Item._Batched).Keys;
+            super.Erase(...Batch);
+            Analytics.Erased = Batch.length;
+        }
+
+        Analytics.Size = super.Size;
+
+        return Analytics;
     }
 
 }
@@ -36,6 +110,7 @@ module.exports = RRQDB;
  * @param {Pathlike} Path Path to the database file for a connection.
  * @param {Boolean} Memory Whether or not to instantly fetch everything from the database.
  * @param {Number} Size Maximum size to keep rolling the entries at.
+ * @param {Number} BatchSize A value to exceed to archieve in batch, due to performance reasons.
  */
 
 /**
